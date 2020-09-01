ae70d9053af076b6ecb30b4269e6b0c867b30a4e
AN0NIT <64950553+AN0NIT@users.noreply.github.com>
2020-09-01 02:43:23 -0400

    Add files via upload
    "Better OS Detection, removed word2string function"

--- wifi_pass.py
+++ wifi_pass.py
@@ -1,3 +1,17 @@
+<<<<<<< HEAD
+import os,sys
+
+if os.name == 'nt':
+    print("[+] Gathering SSID's saved in the system")
+else:
+    print("This script runs only on Windows!")
+    sys.exit()
+
+os.system("echo "" > creds.txt")
+os.system("netsh wlan show profiles > pass.txt")
+
+
+=======
 import subprocess
 
 def string_convert(s):
@@ -29,6 +43,7 @@ subprocess.os.system("echo "" > creds.txt")
 subprocess.os.system("netsh wlan show profiles > pass.txt")
 
 print("[+]Gathering SSID's saved in the system")
+>>>>>>> a3df27150c79f8cf39bdc410aa177067bf90f720
 fhandle = open("pass.txt","r")
 
 for words in fhandle:
@@ -36,22 +51,37 @@ for words in fhandle:
     if "All" in words:
         line = words.split()
         user = line[4]
+<<<<<<< HEAD
+        os.system(f"netsh wlan show profile {user} key=clear >> paste.txt")
+=======
         s = "netsh wlan show profile ",user," key=clear >> paste.txt"
         str = string_convert(s)
         subprocess.os.system(str)
+>>>>>>> a3df27150c79f8cf39bdc410aa177067bf90f720
 
 fhandle.close()
 
 fh = open("paste.txt","r")
 fhand = open("creds.txt","w")
 
+<<<<<<< HEAD
+print("[+] Gathering passwords")
+=======
 print("[+]Gathering passwords")
+>>>>>>> a3df27150c79f8cf39bdc410aa177067bf90f720
 
 for w in fh:
     if "Name" in w or "Content" in w or "Absent" in w:
         line = w.split()
         if "Name" in line:
             user = line[2]
+<<<<<<< HEAD
+            fhand.write(f"    SSID:{user}")
+            fhand.write("\n")
+        elif "Content" in line:
+            passwd = line[3]
+            fhand.write(f"password:{passwd}")
+=======
             s = "    SSID:",user
             str = string_convert(s)
             fhand.write(str)
@@ -61,6 +91,7 @@ for w in fh:
             s = "password:",passwd
             str = string_convert(s)
             fhand.write(str)
+>>>>>>> a3df27150c79f8cf39bdc410aa177067bf90f720
             fhand.write("\n\n")
             continue
         elif "Absent" in line:
@@ -71,7 +102,13 @@ for w in fh:
 fhand.close()
 fh.close()
 
+<<<<<<< HEAD
+print("[!] The credentials are saved in:'creds.txt'")
+os.system("del pass.txt")
+os.system("del paste.txt")
+=======
 print("[+]The credentials are saved in:'creds.txt'")
 subprocess.os.system("del pass.txt")
 subprocess.os.system("del paste.txt")
 subprocess.os.system("del info.txt")
+>>>>>>> a3df27150c79f8cf39bdc410aa177067bf90f720
